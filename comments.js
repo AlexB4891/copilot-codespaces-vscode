876ad4f143fe381782daad6c81c801f3c0f8df17
Alex Bajaña <58230734+AlexB4891@users.noreply.github.com>
2023-11-04 16:45:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,46 +1,68 @@
-// create a web server with Node.js
-// run: node comments.js
-// then: http://localhost:3000
+// Create a web server for comments
+// 
+// 1. load express
+const express = require('express');
+// 2. create an instance of express
+const app = express();
+// 3. load the comments.json file
+const comments = require('./comments.json');
+// 4. load body-parser
+const bodyParser = require('body-parser');
+// 5. load cors
+const cors = require('cors');
 
-var http = require('http');
-var url = require('url');
-var items = [];
+// 6. use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+// 7. use cors
+app.use(cors());
 
-var server = http.createServer(function(req, res){
-	switch(req.method){
-		case 'POST':
-			var item = '';
-			req.setEncoding('utf8');
-			req.on('data', function(chunk){
-				item += chunk;
-			});
-			req.on('end', function(){
-				items.push(item);
-				res.end('OK\n');
-			});
-			break;
-		case 'GET':
-			items.forEach(function(item, i){
-				res.write(i + ') ' + item + '\n');
-			});
-			res.end();
-			break;
-		case 'DELETE':
-			var path = url.parse(req.url).pathname;
-			var i = parseInt(path.slice(1), 10);
-
-			if(isNaN(i)){
-				res.statusCode = 400;
-				res.end('Invalid item id');
-			}else if(!items[i]){
-				res.statusCode = 404;
-				res.end('Item not found');
-			}else{
-				items.splice(i, 1);
-				res.end('OK\n');
-			}
-			break;
-	}
+// 8. create a route for get all comments
+app.get('/comments', (req, res) => { 
+    res.json(comments);
 });
 
-server.listen(3000);
+// 9. create a route for get a single comment
+app.get('/comments/:id', (req, res) => {
+    // 10. find the comment by id
+    const comment = comments.find(comment => comment.id === req.params.id);
+    // 11. return the comment
+    res.json(comment);
+});
+
+// 12. create a route for post a comment
+app.post('/comments', (req, res) => {
+    // 13. create a new comment
+    const comment = {
+        id: Date.now().toString(),
+        name: req.body.name,
+        comment: req.body.comment
+    };
+    // 14. push the new comment to the comments array
+    comments.push(comment);
+    // 15. return the new comment
+    res.json(comment);
+});
+
+// 16. create a route for update a comment
+app.put('/comments/:id', (req, res) => {
+    // 17. find the comment by id
+    const comment = comments.find(comment => comment.id === req.params.id);
+    // 18. update the comment
+    comment.name = req.body.name;
+    comment.comment = req.body.comment;
+    // 19. return the updated comment
+    res.json(comment);
+});
+
+// 20. create a route for delete a comment
+app.delete('/comments/:id', (req, res) => {
+    // 21. find the comment by id
+    const commentIndex = comments.findIndex(comment => comment.id === req.params.id);
+    // 22. delete the comment
+    comments.splice(commentIndex, 1);
+    // 23. return the deleted comment
+    res.json({ message: 'Comment deleted' });
+});
+
+// 24. run the server
+app.listen(5000, () => console.log('Server running on port 5000'));
